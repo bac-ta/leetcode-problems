ee44ad9b2278886022b031d95785ed9b2d2ddf0c
bac-ta <bac93.it@gmail.com>
2023-01-12 14:47:19 +0700

    update

--- src/longest_substring_with_at_most_two_distinct_characters/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ src/longest_substring_with_at_most_two_distinct_characters/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -1,14 +1,28 @@
 package longest_substring_with_at_most_two_distinct_characters;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LongestSubstringWithAtMostTwoDistinctCharacters {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0, j = 0; i < s.length(); i++) {
-
-            if (map.containsKey(s.charAt(i)))
+        int[] track = new int[256];
+        int i = 0, numDistinct = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (track[s.charAt(j)] == 0) {
+                // Not exist
+                numDistinct++;
+            }
+            track[s.charAt(j)]++;
+            while (numDistinct > 2) {
+                track[s.charAt(i)]--;
+                if (track[s.charAt(i)] == 0)
+                    numDistinct--;
+                i++;
+            }
+            maxLen = Math.max(j - i + 1, maxLen);
         }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        var maxLength = new LongestSubstringWithAtMostTwoDistinctCharacters().lengthOfLongestSubstringTwoDistinct("eeeeceba");
+        System.out.println(maxLength);
     }
 }
