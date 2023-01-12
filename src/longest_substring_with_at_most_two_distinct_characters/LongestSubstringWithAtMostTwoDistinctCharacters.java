92b61aac1a1503f3db437e16ba9679da51f07317
bac-ta <bac93.it@gmail.com>
2023-01-12 15:26:45 +0700

    updaste

--- src/longest_substring_with_at_most_two_distinct_characters/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ src/longest_substring_with_at_most_two_distinct_characters/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -3,20 +3,22 @@ package longest_substring_with_at_most_two_distinct_characters;
 public class LongestSubstringWithAtMostTwoDistinctCharacters {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         int[] track = new int[256];
-        int i = 0, numDistinct = 0, maxLen = 0;
-        for (int j = 0; j < s.length(); j++) {
-            if (track[s.charAt(j)] == 0) {
+        int startIndex = 0, numDistinct = 0, maxLen = 0;
+        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
+            if (track[s.charAt(endIndex)] == 0) {
                 // Not exist
                 numDistinct++;
             }
-            track[s.charAt(j)]++;
+            track[s.charAt(endIndex)]++;
             while (numDistinct > 2) {
-                track[s.charAt(i)]--;
-                if (track[s.charAt(i)] == 0)
+                track[s.charAt(startIndex)]--;
+                if (track[s.charAt(startIndex)] == 0)
                     numDistinct--;
-                i++;
+                startIndex++;
             }
-            maxLen = Math.max(j - i + 1, maxLen);
+            System.out.println("start index: " + startIndex);
+            System.out.println("end index: " + endIndex);
+            maxLen = Math.max(endIndex - startIndex + 1, maxLen);
         }
         return maxLen;
     }
