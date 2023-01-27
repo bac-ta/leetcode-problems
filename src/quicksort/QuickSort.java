4ff992be82d4af4ba7aaea37c9c47914a4d41fce
bac-ta <bac93.it@gmail.com>
2023-01-27 09:31:46 +0700

    update quick sort

---
+++ src/quicksort/QuickSort.java
@@ -0,0 +1,41 @@
+package quicksort;
+
+public class QuickSort {
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    //Partition to find pivot
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 100, 7, 8, 9, 1, 5, 101, 98, -7};
+        int n = arr.length;
+
+        new QuickSort().quickSort(arr, 0, n - 1);
+        for (Integer i : arr) {
+            System.out.println(i);
+        }
+    }
+}
