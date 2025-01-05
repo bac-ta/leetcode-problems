848b3e2646edf369dae9a45ee81e5a066e47b635
bactd <bactd@hivelab.com>
2025-01-05 23:22:38 +0700

    Additional condition check rotate array

--- src/rotate_array/RotateArray.java
+++ src/rotate_array/RotateArray.java
@@ -1,22 +1,22 @@
 package rotate_array;
 
-import java.util.Arrays;
-
 public class RotateArray {
     public void rotate(int[] arr, int k) {
+        if (arr == null || arr.length == 0 || k < 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+
         int length = arr.length;
-        System.out.println("length: " + length);
         int t = length - (k % length);
-        System.out.println("t: " + t);
         reverse(arr, 0, t - 1);
-        System.out.println(Arrays.toString(arr));
         reverse(arr, t, length - 1);
-        System.out.println(Arrays.toString(arr));
         reverse(arr, 0, length - 1);
-        System.out.println(Arrays.toString(arr));
     }
 
     private void reverse(int[] arr, int left, int right) {
+        if (arr.length == 1)
+            return;
+
         while (left <= right) {
             int temp = arr[left];
             arr[left] = arr[right];
