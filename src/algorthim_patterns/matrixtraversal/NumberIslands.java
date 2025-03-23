f7fea46d4cd2462ae4ba67e7a2f6c3b9ec0c2767
shin jangcheol <chochehan@hivelab.co.kr>
2025-03-23 23:04:30 +0700

    Additional matrix traversal

---
+++ src/algorthim_patterns/matrixtraversal/NumberIslands.java
@@ -0,0 +1,46 @@
+package algorthim_patterns.matrixtraversal;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NumberIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '0') continue;
+                count++;
+                bfs(grid, i, j);
+            }
+        }
+        return count;
+    }
+
+    private void bfs(char[][] grid, int row, int col) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{row, col});
+        grid[row][col] = '0';
+        while (!queue.isEmpty()) {
+            int[] position = queue.poll();
+            int x = position[0];
+            int y = position[1];
+            for (int[] direction : directions) {
+                int newX = x + direction[0];
+                int newY = y + direction[1];
+                if (newX < 0 || newX >= m || newY < 0 || newY >= n || grid[newX][newY] != '1') continue;
+
+                queue.add(new int[]{newX, newY});
+                grid[newX][newY] = '0';
+            }
+        }
+    }
+
+}
---
+++ src/algorthim_patterns/slidingwindow/MinMaxDiffContinuousSubArrays.java
@@ -0,0 +1,22 @@
+package algorthim_patterns.slidingwindow;
+
+public class MinMaxDiffContinuousSubArrays {
+    public double findMaxAverage(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 0 || k > nums.length) return -1;
+
+        double minSum = Double.POSITIVE_INFINITY;
+        double maxSum = nums[0];
+        double windowSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            windowSum += nums[i];
+            if (i + 1 < k) continue;
+            windowSum = windowSum / k;
+            minSum = Math.min(minSum, windowSum);
+            maxSum = Math.max(maxSum, windowSum);
+            windowSum -= nums[i + 1 - k];
+        }
+
+        return maxSum / k - minSum / k;
+
+    }
+}
