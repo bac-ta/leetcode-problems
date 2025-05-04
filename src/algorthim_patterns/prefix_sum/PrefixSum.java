46d487f4d2d402acbd508cdece678fa1cdb32358
shin jangcheol <chochehan@hivelab.co.kr>
2025-05-04 22:31:16 +0700

    update prefix sum

---
+++ src/algorthim_patterns/prefix_sum/PrefixSum.java
@@ -0,0 +1,20 @@
+package algorthim_patterns.prefix_sum;
+
+public class PrefixSum {
+    public int calculateInRange(int[] nums, int left, int right) {
+        int[] prefixSum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        return calculatePrefixSum(prefixSum, left, right);
+    }
+
+    private int calculatePrefixSum(int[] prefixSum, int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+
+    public static void main(String[] args) {
+        PrefixSum ps = new PrefixSum();
+        System.out.println(ps.calculateInRange(new int[]{1, 2, 3, 4, 110, 24, 34, 67}, 3, 6));
+    }
+}
