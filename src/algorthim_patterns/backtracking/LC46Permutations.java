79007360e3d963aa94768faceb4eb5684db378c8
shin jangcheol <chochehan@hivelab.co.kr>
2025-06-10 22:49:09 +0700

    backtracking lc 46

---
+++ src/algorthim_patterns/backtracking/LC46Permutations.java
@@ -0,0 +1,28 @@
+package algorthim_patterns.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC46Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(results, new ArrayList<>(), nums, used);
+        return results;
+    }
+
+    private void backtrack(List<List<Integer>> results, List<Integer> paths, int[] nums, boolean[] used) {
+        if (paths.size() == nums.length) {
+            results.add(new ArrayList<>(paths));
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            paths.add(nums[i]);
+            used[i] = true;
+            backtrack(results, paths, nums, used);
+            paths.removeLast();
+            used[i] = false;
+        }
+    }
+}
