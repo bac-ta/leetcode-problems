7d05cbb4872d91ac7cd90b20d70311e5301bc99e
shin jangcheol <chochehan@hivelab.co.kr>
2025-06-14 10:02:26 +0700

    Update

--- src/algorthim_patterns/binary_search/LC35SearchInsertPosition.java
+++ src/algorthim_patterns/binary_search/LC35SearchInsertPosition.java
@@ -2,4 +2,25 @@ package algorthim_patterns.binary_search;
 
 public class LC35SearchInsertPosition {
 
+    public int minMaxDifference(int num) {
+        var numStr = String.valueOf(num);
+
+        var firstEl = numStr.charAt(0);
+        var minNumStr = numStr.replaceAll(String.valueOf(firstEl), "0");
+
+        var minNum = Integer.valueOf(minNumStr);
+
+        String maxNumStr = "";
+
+        if (firstEl == '9' && numStr.length() > 1) {
+            maxNumStr = numStr.replaceAll(String.valueOf(numStr.charAt(1)), "9");
+        } else {
+            maxNumStr = numStr.replaceAll(String.valueOf(firstEl), "9");
+        }
+
+        var maxNum = Integer.valueOf(maxNumStr);
+
+        return maxNum - minNum;
+    }
+
 }
