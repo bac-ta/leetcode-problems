51472b4633b6198f1fd3140b7ba6fa2e63e583c0
shin jangcheol <chochehan@hivelab.co.kr>
2025-06-15 21:31:33 +0700

    update code

--- src/algorthim_patterns/others/LC2566MaximumDifferenceByRemappingADigit.java
+++ src/algorthim_patterns/others/LC2566MaximumDifferenceByRemappingADigit.java
@@ -5,19 +5,19 @@ public class LC2566MaximumDifferenceByRemappingADigit {
 
     public int minMaxDifference(int num) {
         var numStr = String.valueOf(num);
-
-        var firstEl = numStr.charAt(0);
-        var minNumStr = numStr.replaceAll(String.valueOf(firstEl), "0");
+        var minNumStr = numStr.replaceAll(String.valueOf(numStr.charAt(0)), "0");
 
         var minNum = Integer.valueOf(minNumStr);
 
-        String maxNumStr = "";
-
-        if (firstEl == '9' && numStr.length() > 1) {
-            maxNumStr = numStr.replaceAll(String.valueOf(numStr.charAt(1)), "9");
-        } else {
-            maxNumStr = numStr.replaceAll(String.valueOf(firstEl), "9");
+        var indexReplace = 0;
+        for (var i = 0; i < numStr.length(); i++) {
+            if (numStr.charAt(i) == '9')
+                continue;
+            indexReplace = i;
+            break;
         }
+
+        var maxNumStr = numStr.replaceAll(String.valueOf(numStr.charAt(indexReplace)), "9");
 
         var maxNum = Integer.valueOf(maxNumStr);
 
