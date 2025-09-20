16080d190788bc2006e764ed13c941c1688b79a6
bac-ta <entropydb@gmail.com>
2025-09-20 11:25:05 +0700

    create leetcode 102

---
+++ src/algorithm_patterns/tree/LC102BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,51 @@
+package algorithm_patterns.tree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LC102BinaryTreeLevelOrderTraversal {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+            super();
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                var node = queue.poll();
+                currentLevel.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+}
