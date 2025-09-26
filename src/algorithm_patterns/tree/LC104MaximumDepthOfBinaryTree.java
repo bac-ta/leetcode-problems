e9c6d656b95c20799d8003f6e1ef53dec5f8e098
bac-ta <entropydb@gmail.com>
2025-09-26 23:24:53 +0700

    update

---
+++ src/algorithm_patterns/tree/LC104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+package algorithm_patterns.tree;
+
+import java.util.List;
+
+public class LC104MaximumDepthOfBinaryTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
