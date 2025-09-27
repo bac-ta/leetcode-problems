84646338f0b1e668d0801d847d5964cab2d7081d
bac-ta <entropydb@gmail.com>
2025-09-27 23:49:58 +0700

    create

---
+++ src/algorithm_patterns/tree/LC110BalancedBinaryTree.java
@@ -0,0 +1,39 @@
+package algorithm_patterns.tree;
+
+public class LC110BalancedBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
